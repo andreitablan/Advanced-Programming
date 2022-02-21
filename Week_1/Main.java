0e1bb345e36b1778987b6adbaf2b946c94dd4c70
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-02-21 18:28:19 +0200

    Update Main.java

--- Week_1/Main.java
+++ Week_1/Main.java
@@ -1,2 +1,29 @@
--
-  
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Family!!! Tab says hi!");
+
+        String[] languages = new String[]{"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+
+        int n = (int) (Math.random() * 1_000_000);
+        System.out.println(n);
+
+        int result=0;
+        result = n * 3;
+
+        int binar=10101;
+        int number=0;
+        int po=0;
+        while(binar!=0) {
+        number= (int) (number+binar%10 * Math.pow(2, po));;
+        po = po+1;
+        binar=binar/10;
+        }
+        System.out.println(number);
+
+        System.out.println("Willy-nilly, this semester I will learn");
+        System.out.println(languages[result]);
+    }
+}
