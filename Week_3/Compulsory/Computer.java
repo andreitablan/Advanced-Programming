ecd3c78ea9a9340622e4ddef4ddcc0055241ee1e
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:06:10 +0200

    Update Computer.java

--- Week_3/Compulsory/Computer.java
+++ Week_3/Compulsory/Computer.java
@@ -1,25 +1,39 @@
-import javax.xml.stream.Location;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Computer{
-    private String address;
-    private int storageCapacity;
+public class Main {
+   public static void main(String[] args){
+      Network N=new Network();
 
-    /*
-    @Override
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    @Override
-    public int getStorageCapacity() {
-        return storageCapacity;
-    }
+      Computer V1=new Computer("v1");
+      V1.setAddress("1223.12");
+      V1.setStorageCapacity(1000);
+      N.addNode(V1);
 
-    @Override
-    public int compareTo(Location o) {
-        return 0;
-    }
-*/
+      Router V2=new Router("v2");
+      V2.setAddress("1234.11");
+      N.addNode(V2);
+
+      Switch V3=new Switch("v3");
+      V3.setAddress("1243.23.1");
+      N.addNode(V3);
+
+      Computer V4=new Computer("v4");
+      V4.setAddress("1223.32.32");
+      V4.setStorageCapacity(120);
+      N.addNode(V4);
+
+      Computer V5=new Computer("v5");
+      V5.setAddress("1223.12.233");
+      V5.setStorageCapacity(100);
+      N.addNode(V5);
+
+      Computer V6=new Computer("v6");
+      V6.setAddress("1223.12.232");
+      V6.setStorageCapacity(200);
+      N.addNode(V6);
+       
+      N.printSimpleNodes();
+
+   }
 }
