c2e266389481f9d399c16a3c68dcd8fdb776c497
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:21:21 +0200

    Update Switch.java

--- Week_3/Compulsory/Switch.java
+++ Week_3/Compulsory/Switch.java
@@ -1,2 +1,25 @@
-public class Switch {
+public class Switch extends Node{
+    private String address;
+
+    /**
+     *The basic constructor for the class.
+     */
+    public Switch(String name) {
+        super(name);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Switch{" +
+                "address='" + address + '\'' +
+                '}' + '\n';
+    }
 }
