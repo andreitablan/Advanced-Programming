517f83b33c4b96943bb18cbd6953ea07562353fd
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:18:36 +0200

    Update Node.java

--- Week_3/Compulsory/Node.java
+++ Week_3/Compulsory/Node.java
@@ -1,17 +1,17 @@
-import javax.xml.stream.Location;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Node {
-    private String name,ip;
-    /*
-    private Map<Node, Integer> cost = new HashMap<>();
-     */
-    public Node(String nume)
-    {
-        this.name=nume;
-    }
+public abstract class Node implements Comparable<Node>{
+    private String name;
+    private Map<Node, Integer> cost= new HashMap<>();
 
+    /**
+     *The basic constructor for the class
+     */
+    public Node(String name) {
+        this.name = name;
+    }
+    
     public String getName() {
         return name;
     }
@@ -20,18 +20,14 @@ public class Node {
         this.name = name;
     }
 
-    /*public Map<Node, Integer> getCost() {
+    public Map<Node, Integer> getCost() {
         return cost;
-    }
-
-    public void setCost(Map<Node, Integer> cost) {
-        this.cost = cost;
     }
 
     public void setCost(Node node, int value) {
         cost.put(node, value);
     }
-
+ 
     @Override
     public String toString() {
         return "Node{" +
@@ -39,12 +35,13 @@ public class Node {
                 ", cost=" + cost +
                 '}';
     }
-
+    
+    /**
+     * The function is used to compare two nodes.
+     */
     @Override
-    public int compareTo(Node other) {
-        return this.name.compareTo(other.name);
-        //not safe, check if name is null before
+    public int compareTo(Node o) {
+        return this.name.compareTo(o.name);
     }
 
-     */
 }
