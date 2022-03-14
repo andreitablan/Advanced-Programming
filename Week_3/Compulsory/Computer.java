c1bcc164ac6b46db8bd38b83998907ddb3e0ebbd
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:13:34 +0200

    Update Computer.java

--- Week_3/Compulsory/Computer.java
+++ Week_3/Compulsory/Computer.java
@@ -1,39 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
 
-public class Main {
-   public static void main(String[] args){
-      Network N=new Network();
+import java.util.Map;
 
-      Computer V1=new Computer("v1");
-      V1.setAddress("1223.12");
-      V1.setStorageCapacity(1000);
-      N.addNode(V1);
+public class Computer extends Node implements Identifiable, Storage{
+    private String address;
+    private int storageCapacity;
 
-      Router V2=new Router("v2");
-      V2.setAddress("1234.11");
-      N.addNode(V2);
+    /**
+     *This is the basic constructor for the class
+     */
+    public Computer(String name) {
+        super(name);
+    }
 
-      Switch V3=new Switch("v3");
-      V3.setAddress("1243.23.1");
-      N.addNode(V3);
+    /**
+     *Setters and Getters
+     */
+    @Override
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    @Override
+    public int getStorageCapacity() {
+        return storageCapacity;
+    }
+    public void setStorageCapacity(int storageCapacity) {
+        this.storageCapacity = storageCapacity;
+    }
 
-      Computer V4=new Computer("v4");
-      V4.setAddress("1223.32.32");
-      V4.setStorageCapacity(120);
-      N.addNode(V4);
+    /**
+     *The method used at sorting
+     */
+    @Override
+    public int compareTo(Node o) {
 
-      Computer V5=new Computer("v5");
-      V5.setAddress("1223.12.233");
-      V5.setStorageCapacity(100);
-      N.addNode(V5);
+        return this.getName().compareTo(o.getName());
+    }
 
-      Computer V6=new Computer("v6");
-      V6.setAddress("1223.12.232");
-      V6.setStorageCapacity(200);
-      N.addNode(V6);
-       
-      N.printSimpleNodes();
-
-   }
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "address='" + address + '\'' +
+                ", storageCapacity=" + storageCapacity +
+                '}' + '\n';
+    }
 }
