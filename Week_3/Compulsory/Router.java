eb3131c4144a3123aa49d860d328ada70af1a751
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:19:47 +0200

    Update Router.java

--- Week_3/Compulsory/Router.java
+++ Week_3/Compulsory/Router.java
@@ -1,2 +1,25 @@
-public class Router {
+public class Router extends Node implements Identifiable{
+    private String address;
+    
+    /**
+     *The basic constructor for the class.
+     */
+    public Router(String name) {
+        super(name);
+    }
+    
+    @Override
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    @Override
+    public String toString() {
+        return "Router{" +
+                "address='" + address + '\'' +
+                '}' +'\n';
+    }
 }
