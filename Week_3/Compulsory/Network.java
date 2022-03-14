a3d1b1aadf24d930e1be4682977060dae5b925af
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-03-14 11:17:05 +0200

    Update Network.java

--- Week_3/Compulsory/Network.java
+++ Week_3/Compulsory/Network.java
@@ -1,27 +1,55 @@
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Network {
-    private List<Node> nodes = new ArrayList<>();
-    public Network()
-    {
+    private List<Node> nodes=new ArrayList<>();
 
+     /**
+     *The basic constructors for the class.
+     */
+    public Network(List<Node> nodes) {
+        this.nodes = nodes;
     }
-    public void addNode(Node node) {
-        nodes.add(node);
+    public Network(){}
+    
+    public List<Node> getNodes() {
+        return nodes;
+    }
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
     }
 
-    public void afisare()
-    {
-        for(int index=0;index<nodes.size();index ++)
-            System.out.println(" " + nodes.get(index).getName());
+    /**
+     * Add a node method.
+     */
+    public void addNode(Node node){
+        this.nodes.add(node);
     }
 
+    /**
+     * Sort method by names.
+     */
+    public void sort(){
+        Collections.sort(this.nodes);
+    }
+
+    /**
+     * Print method for Compulsory
+     */
+    public void printSimpleNodes() {
+        for (int index = 0; index < nodes.size(); index++) {
+            System.out.println(nodes.get(index).getName());
+        }
+    }
+    
     @Override
     public String toString() {
         return "Network{" +
                 "nodes=" + nodes +
-                '}';
+                '}' ;
     }
+    
 
 }
