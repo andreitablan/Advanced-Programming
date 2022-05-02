9f81926ed150c260a61aeb58b3420554576c80d1
andreitablan <76064833+andreitablan@users.noreply.github.com>
2022-05-02 17:41:38 +0300

    Add files via upload

---
+++ Week_9/Compulsory/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+/**
+ * This is the main class.
+ */
+public class Main {
+    public static void main(String args[]) throws SQLException {
+
+    }
+}
